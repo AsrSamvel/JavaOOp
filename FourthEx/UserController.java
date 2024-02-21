9b8e6e4c7a12f4e38f782a710f64b9c4f3f79cd3
Samvel <assamo@bk.ru>
2024-02-21 11:53:39 +0300

    Обобщение

---
+++ FourthEx/UserController.java
@@ -0,0 +1,6 @@
+package FourthEx;
+
+public class UserController <T extends User> {
+
+	void create (String surname, String firstname, String patronymic);
+}
---
+++ FourthEx/UserView.java
@@ -0,0 +1,7 @@
+package FourthEx;
+
+import java.util.List;
+
+public class UserView <T extends User> {
+	public void sendOnConsole(List<T> list);
+}
